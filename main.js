5f082b38dd97dfb752389a83c746d8c704eb680d
Meistter <josueperez723@gmail.com>
2022-12-21 10:43:53 -0400

    metodo get

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 const img = document.getElementById('img1')
 const img2 = document.getElementById('img2')
 const img3 = document.getElementById('img3')
-
+const error = document.getElementById('error')
 const btn = document.getElementById('boton')
 
-const API = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_NKUNKal1REFZQFPdaqX7EmVQXp63CsG668PZNgfWPVojynVwNaZk1UOFk5SqJd3L' 
 const API_KEY = 'api_key=live_NKUNKal1REFZQFPdaqX7EmVQXp63CsG668PZNgfWPVojynVwNaZk1UOFk5SqJd3L'
-const API_FAV = 'https://api.thecatapi.com/v1/favourites?'
+const API = `https://api.thecatapi.com/v1/images/search?limit=3&${API_KEY}` 
+const API_FAV = `https://api.thecatapi.com/v1/favourites?&${API_KEY}`
+
 
 async function changeCat(){
     try{
     const res = await fetch(API)
-    const data = await res.json()
-    img1.src = data[0].url
-    img2.src = data[1].url
-    img3.src = data[2].url
+
+    if (res.status == 200){
+        const data = await res.json()
+        img1.src = data[0].url
+        img2.src = data[1].url
+        img3.src = data[2].url
+    }else {
+        error.innerText = "Hubo un error al cargar los Datos" + res.status
+    }
+    
     } catch{
             console.log('error');
     }
@@ -26,15 +33,29 @@ btn.addEventListener('click', changeCat)
 
 
 async function loadFavorites(){
+
     try{
     const res = await fetch(API_FAV)
-    const data = await res.json()
-    const favimg= document.getElementById('favimg')
-    favimg.src = data[0].url
+   
     
-    } catch{
-            console.log('error');
+        if (res.status == 200){       
+        const data = await res.json()
+        const favimg= document.getElementById('favimg')
+        favimg.src = data[0].url
+        console.log(data[0]);
+    }else {
+        error.innerHTML = "Hubo un error al cargar los Datos, ERROR " + res.status
+    }
+    }catch (e){
+        console.log(e);
     }
     
 }
 loadFavorites()
+
+
+//SOLICITUD DE TIPO POST
+
+async function agregarFavorito(){
+
+}
